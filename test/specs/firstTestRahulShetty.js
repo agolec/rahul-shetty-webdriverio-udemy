d7628b3bb3f4f897b0d92d4f1273cd9c8695cae6
Adam Golec <agolec@nexient.com>
2023-04-28 17:13:17 -0400

    add positive login scenario test to the file firstTestRahulShetty.js

--- test/specs/firstTestRahulShetty.js
+++ test/specs/firstTestRahulShetty.js
@@ -1,13 +1,33 @@
 describe('Ecommerce Application', async() => {
-    it('Login Page Fail', async () => {
-          await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
-        //console.log(await browser.getTitle())
+    xit('Login Page Fail', async () => {
+        await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
         await expect(browser).toHaveTitleContaining('Rahul Shetty Academy')
-
         await $("input[name='username']").setValue('rahulshettyacademy')
-        await browser.pause(3000)
         await $("#username").setValue('secondCSS')
-        await browser.pause(3000)
+        const password = $("//input[@type='password']")
+        await password.setValue("learning")
+        await $("#signInBtn").click()
+        await browser.waitUntil(async () => await $("#signInBtn").getAttribute('value') === 'Sign In',
+        {
+                timeout: 5000,
+                timeoutMsg: 'Expected text to be different after 5 seconds.'
+            
+        })
+       await console.log(await $(".alert-danger").getText())
+       await expect($("p")).toHaveText("(username is rahulshettyacademy and Password is learning)")
+
+    })
+    it("Login Success Page", async () => {
+        await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
+        await expect(browser).toHaveTitleContaining('Rahul Shetty Academy')
+        await $("input[name='username']").setValue('rahulshettyacademy')
+        const password = $("//input[@type='password']")
+        await password.setValue("learning")
+        await $("#signInBtn").click()
+        //after this, we must wait for some element that we know will be on the next page to be present.
+        await $(".btn-primary").waitForExist()
+        await expect(browser).toHaveUrlContaining('shop')
+        await expect(browser).toHaveTitle('ProtoCommerce')
 
     })
     
