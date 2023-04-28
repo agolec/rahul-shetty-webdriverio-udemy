93aecf8c59700f08ac407e2093d49bb8f399885a
Adam Golec <agolec@nexient.com>
2023-04-28 13:07:51 -0400

    added code to set text on the username field from firstTestRahulShetty.js file.

--- test/specs/firstTestRahulShetty.js
+++ test/specs/firstTestRahulShetty.js
@@ -3,6 +3,12 @@ describe('Ecommerce Application', async() => {
           await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
         //console.log(await browser.getTitle())
         await expect(browser).toHaveTitleContaining('Rahul Shetty Academy')
+
+        await $("input[name='username']").setValue('rahulshettyacademy')
+        await browser.pause(3000)
+        await $("#username").setValue('secondCSS')
+        await browser.pause(3000)
+
     })
     
 })
