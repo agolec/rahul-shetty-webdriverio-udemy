0b81cce4710128239c1afcbc9cc9bec2cf4c8a52
Adam Golec <agolec@nexient.com>
2023-05-01 15:37:40 -0400

    added test to uiControls.js

--- test/specs/uiControls.js
+++ test/specs/uiControls.js
@@ -1,7 +1,7 @@
 const expectchai = require('chai').expect
 
 describe('UI Controls', async() => {
-    it("UI Controls Test Suite", async () => {
+    xit("UI Controls Test Suite", async () => {
     await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
     await expect(browser).toHaveTitleContaining('Rahul Shetty Academy')
     await $("input[name='username']").setValue('rahulshettyacademy')
@@ -33,7 +33,24 @@ describe('UI Controls', async() => {
     expectchai(await dropdown.getValue()).to.equal("stud")
     })
 
-    it("Dynamic Dropdown Controls", async()=>{
-
+    xit("Dynamic Dropdown Controls", async() => {
+        await browser.url('https://rahulshettyacademy.com/AutomationPractice/')
+        await $('#autocomplete').setValue('ind')
+        await browser.pause(3000)
+        let items = await $$("[class='ui-menu-item'] div")
+        for(var i = 0 ; i < await items.length;i++)
+        {
+            if(await items[i].getText() === "India"){
+                await items[i].click()
+                await browser.pause(3000)
+            }
+        }
+    })
+    it("checkbox identification", async () => {
+        await browser.url('https://rahulshettyacademy.com/AutomationPractice/')
+        const checkbox = await $$("input[type='checkbox']")
+        await checkbox[1].click()
+        console.log(await checkbox[1].isSelected())
+        console.log(await checkbox[2].isSelected())
     })
 })
