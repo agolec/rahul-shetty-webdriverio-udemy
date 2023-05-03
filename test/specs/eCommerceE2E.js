045280f55ea6757be44c6d3452bd216060eb125a
Adam Golec <agolec@nexient.com>
2023-05-03 09:39:48 -0400

    modify eCommerceE2E.js to start using page object model for shop page.

--- test/specs/eCommerceE2E.js
+++ test/specs/eCommerceE2E.js
@@ -1,4 +1,6 @@
 const expectchai = require('chai').expect
+const loginPage = require('../page-objects-rahul-shetty/loginPage')
+const shopPage = require('../page-objects-rahul-shetty/shopPage')
 
 describe('Ecommerce Application', async () => {
 
@@ -11,20 +13,10 @@ describe('Ecommerce Application', async () => {
         await password.setValue("learning")
         await $("#signInBtn").click()
         //after this, we must wait for some element that we know will be on the next page to be present.
-        const checkoutLink = await $("*=Checkout")
-        await checkoutLink.waitForExist() // *= are used to check element by it's text.
-        const cards = await $$("div[class='card h-100']")
-        //use a loop to traverse through the elements represented in the variable 'cards'. 
-        //Then, inside the loop, use child selectors to find things like text/price/description/etx.
-        for(let i = 0; i < await cards.length; i++){
-            //chain the parent selector 'div[class='card h-100'] to the child class that holds the anchor tag and title.
-            const cardTitle = await cards[i].$('div h4 a')
-            
-            if(products.includes(await cardTitle.getText())){
-                await cards[i].$(".card-footer button").click()
-            }
-        }
-        await checkoutLink.click()
+        await shopPage.checkout.waitForExist() // *= are used to check element by it's text.
+       await shopPage.addProductsToCart(products)
+
+        await shopPage.checkout.click()
         const productPrices = await $$("tr td:nth-child(4) strong")
         //convert strings to integers by using the map function.
         const sumOfProducts = (await Promise.all(await productPrices.map((async (productPrice) => parseInt((await productPrice.getText()).split(".")[1].trim())))))
