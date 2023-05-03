bf351833830e7291143cbc4b09e260a1a864d560
Adam Golec <agolec@nexient.com>
2023-05-03 12:58:09 -0400

    refactor the rest of the reviewpage.js to match POM standards.

--- test/page-objects-rahul-shetty/reviewpage.js
+++ test/page-objects-rahul-shetty/reviewpage.js
@@ -7,6 +7,25 @@ class ReviewPage
     get totalPrice(){
         return $("h3 strong")
     }
+    get checkout(){
+        return $(".btn-success")
+    }
+    get country(){
+        return $("#country")
+    }
+    get ellipsis(){
+        return  $(".lds-ellipsis")
+    }
+    get indiaFromDropdown(){
+        return  $("=India")
+    }
+    get checkoutSubmit(){
+        return $("input[type='submit']")
+    }
+    get checkoutSuccessMessage(){
+        return $('.alert-success')
+    }
+    
     async sumOfProducts(){
         const sumOfProducts = (await Promise.all(await this.productPrices.map((async (productPrice) => parseInt((await productPrice.getText()).split(".")[1].trim())))))
         .reduce((acc,price) => acc+price,0)
@@ -16,8 +35,25 @@ class ReviewPage
         const totalValue = await this.totalPrice.getText()
         const totalIntValue = parseInt(totalValue.split(".")[1].trim())
     }
+    async typeCountry(countryText){
+        await this.country.setValue(countryText)
+    }
+    async waitForEllipsisToDisapear(){
+        await this.ellipsis.waitForExist({reverse: true})
+    }
+    async clickIndiaFromList(){
+        await this.indiaFromDropdown.click()
+    }
+    async clickCheckoutSubmit(){
+        await this.checkoutSubmit.click()
+    }
+    //Assertions
     async assertTotalsMatchWhenAdded(sumOfProducts,totalIntValue){
         await expectchai(sumOfProducts).to.equal(totalIntValue)
     }
+    async assertSuccessMessageAfterCheckout(successMessage){
+        await expect(this.checkoutSuccessMessage).toHaveTextContaining(successMessage)
+    }
+    
 }
 module.exports = new ReviewPage()
