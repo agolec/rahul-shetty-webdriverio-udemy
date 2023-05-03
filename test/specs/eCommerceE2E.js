9936643a670fc38726bad1188031cb6275934656
Adam Golec <agolec@nexient.com>
2023-05-03 09:59:52 -0400

    modify eCommerceE2E.js so it uses shop page object in test.

--- test/specs/eCommerceE2E.js
+++ test/specs/eCommerceE2E.js
@@ -1,6 +1,7 @@
 const expectchai = require('chai').expect
 const loginPage = require('../page-objects-rahul-shetty/loginPage')
 const shopPage = require('../page-objects-rahul-shetty/shopPage')
+const reviewPage = require('../page-objects-rahul-shetty/reviewpage')
 
 describe('Ecommerce Application', async () => {
 
@@ -8,22 +9,21 @@ describe('Ecommerce Application', async () => {
 
         const products = ['Nokia Edge','Blackberry']
         await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
-        await $("input[name='username']").setValue('rahulshettyacademy')
-        const password = $("//input[@type='password']")
-        await password.setValue("learning")
-        await $("#signInBtn").click()
-        //after this, we must wait for some element that we know will be on the next page to be present.
-        await shopPage.checkout.waitForExist() // *= are used to check element by it's text.
-       await shopPage.addProductsToCart(products)
+        await loginPage.Login("rahulshettyacademy","learning")
+        await shopPage.checkout.waitForExist() 
+        await shopPage.addProductsToCart(products)
 
         await shopPage.checkout.click()
-        const productPrices = await $$("tr td:nth-child(4) strong")
+        sumOfProducts = await reviewPage.sumOfProducts()
+        totalIntValue = await reviewPage.totalFormattedPrice()
         //convert strings to integers by using the map function.
-        const sumOfProducts = (await Promise.all(await productPrices.map((async (productPrice) => parseInt((await productPrice.getText()).split(".")[1].trim())))))
-        .reduce((acc,price) => acc+price,0)
+        // const sumOfProducts = (await Promise.all(await productPrices.map((async (productPrice) => parseInt((await productPrice.getText()).split(".")[1].trim())))))
+        // .reduce((acc,price) => acc+price,0)
         
-        const totalValue = await $("h3 strong").getText()
-        const totalIntValue = parseInt(totalValue.split(".")[1].trim())
+        // const totalValue = await $("h3 strong").getText()
+        // const totalIntValue = parseInt(totalValue.split(".")[1].trim())
+
+
         await expectchai(sumOfProducts).to.equal(totalIntValue)
         await $(".btn-success").click()
         await $("#country").setValue("ind")
