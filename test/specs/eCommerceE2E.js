db788b35a342bf3673248e0e27c848e51787a80d
Adam Golec <agolec@nexient.com>
2023-05-03 16:11:13 -0400

    add parameterization to login tests. Add login POM tests pass + fail into eCommerceE2E.js file.

--- test/specs/eCommerceE2E.js
+++ test/specs/eCommerceE2E.js
@@ -2,33 +2,36 @@ const expectchai = require('chai').expect
 const loginPage = require('../page-objects-rahul-shetty/loginPage')
 const shopPage = require('../page-objects-rahul-shetty/shopPage')
 const reviewPage = require('../page-objects-rahul-shetty/reviewpage')
+const fs = require('fs')
+let credentials = JSON.parse(fs.readFileSync('test/testData/LoginTest.json'))
 
 describe('Ecommerce Application', async () => {
+    credentials.forEach(  ({username,password}) => {
+        it('Login Page Fail', async () => {
+            await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
+            await expect(browser).toHaveTitleContaining('Rahul Shetty Academy')
+            await loginPage.Login(username,password)
+            await browser.waitUntil(async () => await loginPage.signIn.getAttribute('value') === 'Sign In',
+            {
+                    timeout: 5000,
+                    timeoutMsg: 'Expected text to be different after 5 seconds.'
+                
+            })
+            await console.log(await loginPage.alert.getText())
+            await expect(loginPage.textInfo).toHaveText("(username is rahulshettyacademy and Password is learning)")
 
-    it('Login Page Fail', async () => {
-        await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
-        await expect(browser).toHaveTitleContaining('Rahul Shetty Academy')
-        await loginPage.Login("blah","blah2")
-        await browser.waitUntil(async () => await loginPage.signIn.getAttribute('value') === 'Sign In',
-        {
-                timeout: 5000,
-                timeoutMsg: 'Expected text to be different after 5 seconds.'
-            
         })
-       await console.log(await loginPage.alert.getText())
-       await expect(loginPage.textInfo).toHaveText("(username is rahulshettyacademy and Password is learning)")
 
-    })
+        xit("Login Success Page", async () => {
+            await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
+            await expect(browser).toHaveTitleContaining('Rahul Shetty Academy')
+            await loginPage.Login("rahulshettyacademy","learning")
+            //after this, we must wait for some element that we know will be on the next page to be present.
+            await shopPage.checkout.waitForExist()
+            await expect(browser).toHaveUrlContaining('shop')
+            await expect(browser).toHaveTitle('ProtoCommerce')
 
-    it("Login Success Page", async () => {
-        await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
-        await expect(browser).toHaveTitleContaining('Rahul Shetty Academy')
-        await loginPage.Login("rahulshettyacademy","learning")
-        //after this, we must wait for some element that we know will be on the next page to be present.
-        await shopPage.checkout.waitForExist()
-        await expect(browser).toHaveUrlContaining('shop')
-        await expect(browser).toHaveTitle('ProtoCommerce')
-
+        })
     })
 
     xit('End to End test', async () => {
