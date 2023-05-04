9fe98d16476e5de62a17746ea6b22c2decb4e276
Adam Golec <agolec@nexient.com>
2023-05-04 16:40:52 -0400

    create nexport login modal page object.

---
+++ test/page-objects-nexport/nexportLoginModal.js
@@ -0,0 +1,43 @@
+class LoginModal
+{
+
+get userName()
+{
+    return $("input[name='username']")
+}
+
+get password()
+{
+    return $("input[name='password']")
+}
+
+get loginForm(){
+    return $("form[name='Login']")
+}
+get loginFormHeaderText(){
+    return $('div.md-toolbar-tools h3')
+}
+get enterButton(){
+    return $("button[type='submit']")
+}
+
+
+//actions
+async login(userName,password)
+{
+    await this.userName.setValue(userName)
+    await this.password.setValue(password)
+    await this.enterButton.click()
+}
+
+async assertFormDisplayed(){
+    await expect(this.loginForm).toBeDisplayed()
+}
+
+async assertFormHeaderText(){
+    await expect(this.loginFormHeaderText).toHaveText('Welcome to NexPort')
+}
+
+}
+
+module.exports = new LoginModal()
