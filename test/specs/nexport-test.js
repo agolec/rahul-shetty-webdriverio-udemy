12964a58ea314f394abfb5fc06687a7d5ce927e1
Adam Golec <agolec@nexient.com>
2023-05-04 15:15:27 -0400

    refactor nexport-test.js to use POM further by validating the banner text on the login page.

--- test/specs/nexport-test.js
+++ test/specs/nexport-test.js
@@ -2,19 +2,20 @@ const expectchai = require('chai').expect
 const loginPage = require('../page-objects-nexport/nexportLoginPage')
 
 //const fs = require('fs')
-//let credentials = JSON.parse(fs.readFileSync('test/testData/LoginTestNexport.json'))
+//let loginData = JSON.parse(fs.readFileSync('test/testData/LoginTestNexport.json'))
 
 describe('Nexport Automation', async () => {
    
         it('Login Page', async () => {
             await loginPage.NavigateToPage()
             await loginPage.assertTitle()
-            const banner = await $('p.banner-memo')
+            //const banner = await $('p.banner-memo')
             const loginLink = await $('div.logoutInfo a')
-            await expect(banner).toHaveText('Please note: Nexport Timesheets are due on Friday, '+
-                                      'December 23rd, 2022 by EOD, for the end of the '+
-                                      'December month, as Nexport will shutdown for the' +
-                                      ' NTT Data SAP Migration.')
+            // await expect(banner).toHaveText('Please note: Nexport Timesheets are due on Friday, '+
+            //                           'December 23rd, 2022 by EOD, for the end of the '+
+            //                           'December month, as Nexport will shutdown for the' +
+            //                           ' NTT Data SAP Migration.')
+            await loginPage.assertBannerMemo()
             await loginLink.click()
             //move to login modal.
             const loginModal = $("form[name='Login']")
