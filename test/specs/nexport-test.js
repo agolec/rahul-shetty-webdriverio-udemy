d7a833179ff4c43514a0a63f62610c620f072f50
Adam Golec <agolec@nexient.com>
2023-05-04 16:40:32 -0400

    create test file in specs directory for nexport tests.

--- test/specs/nexport-test.js
+++ test/specs/nexport-test.js
@@ -1,31 +1,28 @@
 const expectchai = require('chai').expect
 const loginPage = require('../page-objects-nexport/nexportLoginPage')
+const loginMod = require('../page-objects-nexport/nexportLoginModal')
 
-//const fs = require('fs')
-//let loginData = JSON.parse(fs.readFileSync('test/testData/LoginTestNexport.json'))
+const fs = require('fs')
+let loginData = JSON.parse(fs.readFileSync('test/testData/LoginTestDataNexport.json'))
+const loginCredentials = require('../testData/loginCredentials')
+
 
 describe('Nexport Automation', async () => {
-   
+    loginData.forEach(  ({bannerMemoText}) => {
         it('Login Page', async () => {
             await loginPage.NavigateToPage()
             await loginPage.assertTitle()
             //const banner = await $('p.banner-memo')
             const loginLink = await $('div.logoutInfo a')
-            // await expect(banner).toHaveText('Please note: Nexport Timesheets are due on Friday, '+
-            //                           'December 23rd, 2022 by EOD, for the end of the '+
-            //                           'December month, as Nexport will shutdown for the' +
-            //                           ' NTT Data SAP Migration.')
-            await loginPage.assertBannerMemo()
+            await loginPage.assertBannerMemo(bannerMemoText)
             await loginLink.click()
             //move to login modal.
-            const loginModal = $("form[name='Login']")
-            await expect(loginModal).toBeDisplayed()
-            const loginModalHeader = await $('div.md-toolbar-tools h3')
-           await expect(loginModalHeader).toHaveText('Welcome to NexPort')
+            await loginMod.assertFormDisplayed()
+            await loginMod.assertFormHeaderText()
+            await loginMod.login(loginCredentials.username,loginCredentials.password)
+            await browser.pause(3000)
            
-
-
         })
-
+    })
   
 })
