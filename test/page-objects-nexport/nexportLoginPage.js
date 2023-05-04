855d86d8edd2ddef2ddd630b012b2bf163119a68
Adam Golec <agolec@nexient.com>
2023-05-04 15:14:50 -0400

    add method to validate nexport banner into page object. Refactor test to call page object for it.

--- test/page-objects-nexport/nexportLoginPage.js
+++ test/page-objects-nexport/nexportLoginPage.js
@@ -11,27 +11,38 @@ get password()
     return $("#password")
 }
 
-get signIn()
-{
-    return $("#signInBtn")
-}
-get alert()
-{
-    return $('.alert-danger')
-}
-get textInfo()
-{
-    return $('p')
+// get signIn()
+// {
+//     return $("#signInBtn")
+// }
+// get alert()
+// {
+//     return $('.alert-danger')
+// }
+// get textInfo()
+// {
+//     return $('p')
+// }
+get bannerMemo(){
+    return $('p.banner-memo')
 }
 
+//actions
 async Login(userName,password)
 {
     await this.userName.setValue(userName)
     await this.password.setValue(password)
     await this.signIn.click()
 }
+
 async NavigateToPage(){
     await browser.url('https://portal.nexient.com/#!/home')
+}
+async assertBannerMemo(){
+expect(this.bannerMemo).toHaveText('Please note: Nexport Timesheets are due on Friday, '+
+                                      'December 23rd, 2022 by EOD, for the end of the '+
+                                      'December month, as Nexport will shutdown for the' +
+                                      ' NTT Data SAP Migration.')
 }
 async assertTitle(){
     await expect(browser).toHaveTitleContaining('Nexient: Employee Portal')
