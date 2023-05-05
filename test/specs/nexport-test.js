fcab9d272a8449821b7e586b770c1d99990ca580
Adam Golec <agolec@nexient.com>
2023-05-05 15:40:37 -0400

    split it() methods into different methods based off page they perform testing on. Add POM and data parameterization into both login page and home page.

--- test/specs/nexport-test.js
+++ test/specs/nexport-test.js
@@ -1,9 +1,11 @@
 const expectchai = require('chai').expect
 const loginPage = require('../page-objects-nexport/nexportLoginPage')
-const loginMod = require('../page-objects-nexport/nexportLoginModal')
+const loginModal = require('../page-objects-nexport/nexportLoginModal')
+const homePage = require('../page-objects-nexport/nexportHomePage')
 
 const fs = require('fs')
 let loginData = JSON.parse(fs.readFileSync('test/testData/LoginTestDataNexport.json'))
+let homePageData = JSON.parse(fs.readFileSync('test/testData/HomePageDataNexport.json'))
 const loginCredentials = require('../testData/loginCredentials')
 
 
@@ -17,12 +19,18 @@ describe('Nexport Automation', async () => {
             await loginPage.assertBannerMemo(bannerMemoText)
             await loginLink.click()
             //move to login modal.
-            await loginMod.assertFormDisplayed()
-            await loginMod.assertFormHeaderText()
-            await loginMod.login(loginCredentials.username,loginCredentials.password)
-            await browser.pause(3000)
-           
+            await loginModal.assertFormDisplayed()
+            await loginModal.assertFormHeaderText()
+            await loginModal.login(loginCredentials.username,loginCredentials.password)
+        //     await homePage.assertBannerMemo('Please note: Nexport Timesheets are due on Friday, December 23rd, 2022 by EOD, for the end of the December month, as Nexport will shutdown for the NTT Data SAP Migration.')
+        //    await browser.pause(2000)
         })
     })
+    homePageData.forEach(({homePageBannerMemo}) => {
+        it('HomePage', async () => {
+            await homePage.assertBannerMemo(homePageBannerMemo)
+            await browser.pause(2000)
+        })
+})
   
 })
