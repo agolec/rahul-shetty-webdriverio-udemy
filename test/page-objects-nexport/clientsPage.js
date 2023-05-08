e8fc1219a5709f40865bc69e8bab1f39271290c0
Adam Golec <agolec@nexient.com>
2023-05-08 00:12:01 -0400

    parameterize method that clicks on a client by aria-label value

--- test/page-objects-nexport/clientsPage.js
+++ test/page-objects-nexport/clientsPage.js
@@ -2,17 +2,33 @@
 
 class ClientsPage 
 {
+    
     get clientsList (){
-        return $("md-sidenav [layout='column'].md-list-item-text span")
+        return $$("span.md-subhead")
     }
     get specificClient(){
         //$('span=')
-        return $('=AccessOne')
+        return $('span.md-subhead')
     }
-    async clickClient(){
-        //await this.clientsList[3].this.specificClient()
-        await this.clientsList.selectByVisibleText('AccesOne')
+    
+    get anchors(){
+        return $$('div.md-button a')
     }
+    targetClient(param){
+        //return $("[aria-label='AccessOne']")
+        return $(param)
+    }
+    //prototype for element to click on a specific client?
+    async clickClient(clientAriaLbl){
+        await this.specificClient.waitForExist({timeout: 6000})
+        //could not make this work through a named method.
+        await $(clientAriaLbl).click()
+        await browser.pause(4000)
+    }
+    // async clickClient(){
+    //     //await this.clientsList[3].this.specificClient()
+    //     await console.log(await this.clientsList[4].getText())
+    // }
 }
 
 module.exports = new ClientsPage()
